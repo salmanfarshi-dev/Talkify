0fd8acca064aba1c94a12706e316903771ada469
Salman Farshi <sfarshi222@gmail.com>
2026-06-09 15:29:47 +0600

    feat : add two pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {createRoutesFromElements, createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import Registration from './Pages/Registration';
+import Login from './Pages/Login';
+
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+   <Route>
+
+    <Route path='/' element={ <Registration/> }/>
+    <Route path='login' element={ <Login/> }/>
+
+
+
+   </Route>
+  )
+);
+
 
 function App() {
   return (
-    <div>App</div>
+    <RouterProvider router={router} />
   )
 }
 
